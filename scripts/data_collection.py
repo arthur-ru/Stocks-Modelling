1a3926e22f1b05676f593a455d66db25446e6b09
arthu <arthur.rubio@etu.mines-ales.fr>
2023-10-27 13:59:34 +0200

    essai de correction des indices changeants - Arthur

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -24,6 +24,26 @@ for row in table.find_all("tr")[1:]:  # Commencez à partir de la deuxième lign
         symbol = columns[0].text.strip()  # La première colonne contient les symboles
         sp500_symbols.append(symbol)
 
-# Afficher les symboles
+# Période de données historiques (depuis le 1er janvier 2020)
+start_date = "2020-01-01"
+end_date = "2023-01-01"
+
+# Téléchargement des données
+data = yf.download(sp500_symbols, start=start_date, end=end_date, group_by="ticker")
+
+# Créez une liste pour stocker les symboles invalides
+invalid_symbols = []
+
 for symbol in sp500_symbols:
-    print(symbol)
+    try:
+        # Essayez de télécharger les données pour chaque symbole
+        data[symbol]
+    except Exception as e:
+        print(f"Erreur pour le symbole {symbol}: {e}")
+        invalid_symbols.append(symbol)
+
+# Sauvegarde des données dans un fichier CSV, index=False pour ne pas enregistrer l'index
+data.to_csv("data/raw_data/data_stock.csv", index=False)
+
+# Affichez la liste des symboles invalides
+print("Symboles invalides :", invalid_symbols)
